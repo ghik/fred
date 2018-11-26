2a13bd4c1e5690191770fea0a604307e6e822115
ghik <romeqjanoosh@gmail.com>
2018-11-26 14:36:16 +0100

    udash rest & monix dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,6 +5,8 @@ object Dependencies {
   // It's good to extract dependency version numbers to named values.
   val silencerVersion = "1.2.1"
   val avsCommonsVersion = "1.34.4"
+  val udashRestVersion = "0.8.0-M8"
+  val monixVersion = "3.0.0-RC2"
 
   // List of dependencies containing cross dependencies (for both JVM and JS, using %%% syntax) must be wrapped
   // into `Def.setting` because %%% can't be expanded until it's known whether the project is a JVM or JS project.
@@ -12,6 +14,8 @@ object Dependencies {
     "com.github.ghik" %% "silencer-lib" % silencerVersion,
     // the invented-here commons library
     "com.avsystem.commons" %%% "commons-core" % avsCommonsVersion,
+    "io.udash" %%% "udash-rest" % udashRestVersion,
+    "io.monix" %%% "monix" % monixVersion,
     // compiler plugin for selective warning suppression, which Scala doesn't have by default
     compilerPlugin("com.github.ghik" %% "silencer-plugin" % silencerVersion),
     // the invented-here static code analyzer with some random rules like rejecting `import java.util`
